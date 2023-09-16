92d8db4ca41287d46833fc96938397630565a33d
V-Yehor <130846410+V-Yehor@users.noreply.github.com>
2023-09-16 15:07:32 +0000

    Deploying to gh-pages from @ V-Yehor/goit-js-hw-09@ce01852185200adc66e84b8e5d263c8db60afa78 🚀

---
+++ 01-color-switcher.a21e0091.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),d=document.querySelector("body");let r;t.setAttribute("disabled","disabled"),e.addEventListener("click",(()=>{e.setAttribute("disabled","disabled"),t.removeAttribute("disabled"),r=setInterval((()=>{d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),t.addEventListener("click",(()=>{e.removeAttribute("disabled"),t.setAttribute("disabled","disabled"),clearInterval(r)}));
+//# sourceMappingURL=01-color-switcher.a21e0091.js.map
